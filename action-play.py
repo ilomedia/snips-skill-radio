0345ca493407cf041ebda3ff2e815131d3dc0604
ilomedia <contact@ilomedia.net>
2019-06-25 18:31:14 +0200

    play

--- action-play.py
+++ action-play.py
@@ -1,19 +1,40 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+
+import ConfigParser
 from hermes_python.hermes import Hermes
+from hermes_python.ffi.utils import MqttOptions
+from hermes_python.ontology import *
+import io
 import subprocess
 
-MQTT_IP_ADDR = "localhost"
-MQTT_PORT = 1883
-MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
+CONFIGURATION_ENCODING_FORMAT = "utf-8"
+CONFIG_INI = "config.ini"
+
+class SnipsConfigParser(ConfigParser.SafeConfigParser):
+    def to_dict(self):
+        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
 
 
-def intent_received(hermes, intent_message):
+def read_configuration_file(configuration_file):
+    try:
+        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.readfp(f)
+            return conf_parser.to_dict()
+    except (IOError, ConfigParser.Error) as e:
+        return dict()
+
+def subscribe_intent_callback(hermes, intentMessage):
+    conf = read_configuration_file(CONFIG_INI)
+    action_wrapper(hermes, intentMessage, conf)
+
+
+def action_wrapper(hermes, intentMessage, conf):
+    current_session_id = intentMessage.session_id
+    hermes.publish_end_session(current_session_id, "")
 
     subprocess.Popen(['amixer', '-c', '1', 'sset', '"PCM"', '50%'])
-
-#    c = radiopy.Player('PCM')
-#    c.play("Radio Algérie Internationale")
 
     pid = None
     pid = subprocess.Popen(['mplayer', '-quiet', 'http://rai.ice.infomaniak.ch/rai-64.aac?type=.flv']).pid
@@ -25,5 +46,8 @@ def intent_received(hermes, intent_message):
     print("pid: " + str(pid))
 
 
-with Hermes(MQTT_ADDR) as h:
-    h.subscribe_intents(intent_received).start()
+if __name__ == "__main__":
+    mqtt_opts = MqttOptions()
+    with Hermes(mqtt_options=mqtt_opts) as h:
+        h.subscribe_intent("{{intent_id}}", subscribe_intent_callback) \
+         .start()
