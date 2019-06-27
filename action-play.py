62a39611dc8a4915cdee9fee5613854453d367c0
ilomedia <contact@ilomedia.net>
2019-06-27 09:48:53 +0200

    split actions to avoid intent register bug

---
+++ action-play.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import ConfigParser
+from hermes_python.hermes import Hermes
+from hermes_python.ffi.utils import MqttOptions
+from hermes_python.ontology import *
+import io
+import subprocess
+
+CONFIGURATION_ENCODING_FORMAT = "utf-8"
+CONFIG_INI = "config.ini"
+
+
+class SnipsConfigParser(ConfigParser.SafeConfigParser):
+    def to_dict(self):
+        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
+
+
+def read_configuration_file(configuration_file):
+    try:
+        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.readfp(f)
+            return conf_parser.to_dict()
+    except (IOError, ConfigParser.Error) as e:
+        return dict()
+
+
+def subscribe_intent_callback(hermes, intent_message):
+    conf = read_configuration_file(CONFIG_INI)
+    action_wrapper(hermes, intent_message, conf)
+
+
+def action_wrapper(hermes, intent_message, conf):
+
+    hermes.publish_end_session(intent_message.session_id, "")
+
+    subprocess.Popen(['amixer', '-c', '1', 'sset', '"PCM"', '50%'])
+
+    pid = None
+    pid = subprocess.Popen(['mplayer', '-quiet', 'http://rai.ice.infomaniak.ch/rai-64.aac?type=.flv']).pid
+
+    fpid = open("/tmp/mplayer-id", "w+")
+    fpid.write(str(pid))
+    fpid.close()
+
+    print("pid: " + str(pid))
+
+
+if __name__ == "__main__":
+    mqtt_opts = MqttOptions()
+    with Hermes(mqtt_options=mqtt_opts) as h:
+        h.subscribe_intent("duch:play", subscribe_intent_callback).start()
+
